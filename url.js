c6005919a96994755731dc04ef71a4e3dc17db41
karthik605 <112937219+karthik605@users.noreply.github.com>
2024-06-11 14:20:58 +0530

    Update url.js

--- url.js
+++ url.js
@@ -1,3 +1,3 @@
 // module.exports = `mongodb://localhost:27017`
-module.exports = "mongodb+srv://admin:admin@mdb.vtkja.mongodb.net/"
-// module.exports = `mongodb+srv://admin:<password>@nodedb.0iwtl9y.mongodb.net/`
+// module.exports = "mongodb+srv://admin:admin@mdb.vtkja.mongodb.net/"
+module.exports = `mongodb+srv://admin:admin@nodedb.0iwtl9y.mongodb.net/`
